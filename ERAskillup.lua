fdf84ffee361cddd7a8103a547985a38ed34bc0f
phoen <phoen@JESSE-PC>
2025-10-12 10:33:00 -0400

    Remove GEO Remove Skillup Items Remove /heal commands

--- ERAskillup.lua
+++ ERAskillup.lua
@@ -1,6 +1,6 @@
 packets = require('packets')
 res = require 'resources'
---version 2.0.0.0 final
+--version 20251015
 ----------USER IN CODE SETTINGS----------
 ---Put the spells you want to use in these tables
 ---Example: for healing Healing = T{'Cure','Cure II'},
@@ -9,7 +9,6 @@ res = require 'resources'
 user_settings = {
     user_spells = {
         Healing = T{},
-        Geomancy = T{},
         Enhancing = T{},
         Ninjutsu = T{},
         Singing = T{},
@@ -29,21 +28,14 @@ sets.Idle = {
     legs="Temachtiani Pants",
     feet="Temachtiani Boots",
     }
-sets.Resting = {
-    main="Dark Staff",
-    left_ear="Relaxing Earring",
-    ammo="Clarus Stone",
-    neck="Eidolon pendant",
-    right_ear="Boroka Earring",
-    }
 --DO NOT CHANGE ANY THING BELOW THIS LINE--
 function get_sets()
     skilluprun = false
-    gs_skill = {skillup_table = {"Healing","Geomancy","Enhancing","Ninjutsu","Singing","Blue","Summoning"},skillup_type = 'None',skillup_spells = T{},
-        skillup_count=1,bluspellulid = {['Harden Shell']=737,['Pyric Bulwark']=741,['Carcharian Verve']=745},skill_up_item = T{5889,5890,5891,5892}}
+    gs_skill = {skillup_table = {"Healing","Enhancing","Ninjutsu","Singing","Blue","Summoning"},skillup_type = 'None',skillup_spells = T{},
+        skillup_count=1,bluspellulid = {['Harden Shell']=737,['Pyric Bulwark']=741,['Carcharian Verve']=745}}
     end_skillup = {shutdown = false,logoff = false,stoptype = "Stop"}
-    gs_skillup = {color={GEO=true,HEL=true,ENH=true,NIN=true,SIN=true,BLU=true,SMN=true,STOP=true,DOWN=true,LOG=true,TRUST=true,TEST=true,REF=true,ITEM=true},
-                skill_ups={},total_skill_ups=0,skill={},use_trust=false,use_item=false,use_geo=false,test_mode=false,test_brd="Wind",skipped_spells=T{}}
+    gs_skillup = {color={HEL=true,ENH=true,NIN=true,SIN=true,BLU=true,SMN=true,STOP=true,DOWN=true,LOG=true,TEST=true,REF=true},
+                skill_ups={},total_skill_ups=0,skill={},test_mode=false,test_brd="Wind",skipped_spells=T{}}
     gs_skillup.box={pos={x=211,y=402},text={font='Segoe UI Symbol',size=12,Fonts={'sans-serif'},},bg={alpha=255}}
     gs_skillup.boxa={pos={x=gs_skillup.box.pos.x - 145,y=gs_skillup.box.pos.y},text={font='Segoe UI Symbol',size=9},bg={alpha=255}}
     if gearswap.pathsearch({'Saves/skillup_data.lua'}) then
@@ -93,13 +85,6 @@ function filtered_action(spell)
             send_command('input /ma "'..gs_skill.skillup_spells[gs_skill.skillup_count]..'" <me>')
         end
         return
-    elseif spell.skill == "Ninjutsu" then
-        local tool = nin_tool_open(spell)
-        cancel_spell()
-        if tool then
-            send_command('input /item "'..tool..'" <me>')
-        end
-        return
     elseif S{"Avatar's Favor","Elemental Siphon"}:contains(spell.en) then
         cancel_spell()
         send_command('input /ja "'..res.job_abilities[90][gearswap.language]..'" <me>')
@@ -116,10 +101,6 @@ end
 function precast(spell)
     if pet.isvalid and S{"Release","Full Circle"}:contains(spell.en) then
         if spell.en == "Release" then
-            if not pet.isvalid then
-                cancel_spell()
-                send_command('input /heal on')
-            end
             local recast = windower.ffxi.get_ability_recasts()[spell.recast_id]
             if (recast > 0) then
                 cancel_spell()
@@ -129,10 +110,6 @@ function precast(spell)
         else
             return
         end
-    elseif not pet.isvalid and S{"Release","Avatar's Favor","Elemental Siphon"}:contains(spell.en) then
-        cancel_spell()
-        send_command('input /heal on')
-        return
     end
     if check_skill_cap() or not skilluprun then cancel_spell() shutdown_logoff() return end
     if gs_skill.skillup_type == "Singing" then
@@ -156,50 +133,6 @@ function precast(spell)
             return
         end
     end
-    if spell and (spell.mp_cost + 25) > player.mp  then
-        if gs_skillup.skipped_spells:contains(spell.name) then
-            gs_skillup.skipped_spells:clear()
-            cancel_spell()
-            send_command('input /heal on')
-            return
-        end
-        cancel_spell()
-        gs_skillup.skipped_spells:append(spell.name)
-        gs_skill.skillup_count = (gs_skill.skillup_count % #gs_skill.skillup_spells) + 1
-        send_command('input /ma "'..gs_skill.skillup_spells[gs_skill.skillup_count]..'" <me>')
-        return
-    end
-    if gs_skillup.use_trust and party.count == 1 and spell_usable(res.spells[931]) then
-        if spell.en ~= "Moogle" then
-            cancel_spell()
-            send_command('input /ma "'..res.spells[931][gearswap.language]..'" <me>')
-            return
-        end
-    end
-    if gs_skillup.use_item and spell.type ~= "Item" and not buffactive[251] then
-        for i,v in ipairs(gs_skill.skill_up_item) do
-            if player.inventory[res.items[v][gearswap.language]] then
-                cancel_spell()
-                send_command('input /item "'..res.items[v][gearswap.language]..'" <me>')
-                return
-            end
-        end
-    elseif gs_skillup.use_geo then
-        if player.main_job == "GEO" and spell_usable(res.spells[800]) and not pet.isvalid then
-            if spell.en ~= "Geo-Refresh" then
-                cancel_spell()
-                send_command('input /ma "'..res.spells[800][gearswap.language]..'" <me>')
-                return
-            end
-            return
-        elseif player.sub_job == "GEO" and spell_usable(res.spells[770]) and buffactive[541] ~= (gs_skillup.use_trust and 2 or 1) then
-            if spell.en ~= "Indi-Refresh" then
-                cancel_spell()
-                send_command('input /ma "'..res.spells[770][gearswap.language]..'" <me>')
-                return
-            end
-        end
-    end
     if spell.name == gs_skill.skillup_spells[gs_skill.skillup_count] then
         if not spell_usable(spell) then
             cancel_spell()
@@ -211,14 +144,6 @@ function precast(spell)
     end
 end
 function aftercast(spell)
-    if pet.isvalid and check_skill_cap() then
-        if pet.name == "Luopan" then
-            send_command('wait 1.0;input /ja "'..res.job_abilities[345][gearswap.language]..'" <me>')
-        else
-            send_command('wait 1.0;input /ja "'..res.job_abilities[90][gearswap.language]..'" <me>')
-        end
-        return
-    end
     if check_skill_cap() then
         shutdown_logoff() return 
     elseif spell.interrupted then
@@ -259,7 +184,7 @@ function self_command(command)
                 if #gs_skill.skillup_spells > 0 then
                     gs_skill.skillup_spells:clear()
                 end
-                local skill_id = {["Healing"]=33,["Enhancing"]=34,["Summoning"]=38,["Ninjutsu"]=39,["Singing"]=40,["Blue"]=43,["Geomancy"]=44}
+                local skill_id = {["Healing"]=33,["Enhancing"]=34,["Summoning"]=38,["Ninjutsu"]=39,["Singing"]=40,["Blue"]=43}
                 local spells_have = windower.ffxi.get_spells()
                 for i,v in pairs(res.spells) do
                     if v.skill == skill_id[gs_skill.skillup_type] and spell_valid(v) and spells_have[v.id] then
@@ -295,13 +220,7 @@ function self_command(command)
         end_skillup.stoptype = "Stop"
         end_skillup.shutdown = false
         end_skillup.logoff = false
-    elseif command == 'settrust' then
-        gs_skillup.use_trust = not gs_skillup.use_trust
-    elseif command == 'setitem' then
-        gs_skillup.use_item = not gs_skillup.use_item
-    elseif command == 'setgeo' then
-        gs_skillup.use_geo = not gs_skillup.use_geo
-    elseif command == 'changeinstrament' then
+    elseif command == 'changeinstrument' then
         gs_skillup.test_brd = (gs_skillup.test_brd=="Wind" and "String" or "Wind")
     end
     initialize(window, gs_skillup.box, 'window')
@@ -323,11 +242,6 @@ function check_skill_cap()
             skilluprun = false
             return true
         end
-    elseif gs_skill.skillup_type == "Geomancy" then
-        if gs_skillup.skill['Geomancy Capped'] and gs_skillup.skill['Handbell Capped'] and not gs_skillup.test_mode then
-            skilluprun = false
-            return true
-        end
     elseif gs_skill.skillup_type == "Singing" then
         if gs_skillup.skill['Singing Capped'] and gs_skillup.skill['Stringed Instrument Capped'] and gs_skillup.skill['Wind Instrument Capped'] and not gs_skillup.test_mode then
             skilluprun = false
@@ -339,7 +253,7 @@ function check_skill_cap()
 end
 function spell_valid(tab)
     if (tab.levels[player.main_job_id] and tab.levels[player.main_job_id] <= player.main_job_level or tab.levels[player.sub_job_id] and tab.levels[player.sub_job_id] <= player.main_job_level) and tab.targets:contains('Self') and
-        not tab.en:wmatch('Teleport-*|Warp*|Tractor*|Retrace|Escape|Geo-*|Sacrifice|Odin|Alexander|Recall-*') then
+        not tab.en:wmatch('Teleport-*|Warp*|Tractor*|Retrace|Escape|Sacrifice|Odin|Alexander|Recall-*') then
         return true
     end
 end
@@ -353,29 +267,6 @@ function shutdown_logoff()
     initialize(window, gs_skillup.box, 'window')
     updatedisplay()
 end
-function nin_tool_open(spell)
-    local bag_id = {['Sanjaku-Tenugui']=5417,['Soshi']=5734,['Uchitake']=5308,['Tsurara']=5309,['Kawahori-Ogi']=5310,['Makibishi']=5311,['Hiraishin']=5312,
-        ['Mizu-Deppo']=5313,['Shihei']=5314,['Jusatsu']=5315,['Kaginawa']=5316,['Sairui-Ran']=5317,['Kodoku']=5318,['Shinobi-Tabi']=5319,['Ranka']=6265,
-        ['Furusumi']=6266,['Kabenro']=5863,['Jinko']=5864,['Ryuno']=5865,['Mokujin']=5866,["Chonofuda"]=5869,["Inoshishinofuda"]=5867,["Shikanofuda"]=5868}
-    local t = gearswap.tool_map[spell.en].en
-    local ut = gearswap.universal_tool_map[spell.en].en
-    local tb = res.items[bag_id[t]][gearswap.language]
-    local utb = res.items[bag_id[ut]][gearswap.language]
-    if #gs_skill.skillup_spells > 0 then
-        if player.inventory[tb] ~= nil then
-            return tb
-        elseif player.inventory[utb] ~= nil then
-            return utb
-        else
-            gs_skill.skillup_spells:delete(spell.name)
-            return false
-        end
-    else
-        skilluprun = false
-        cancel_spell()
-        add_to_chat(7,"No Tools Available To Cast Any Ninjutsu")
-    end
-end
 function initialize(text, settings, a)
     if a == 'window' then
         local properties = L{}
@@ -383,24 +274,11 @@ function initialize(text, settings, a)
             properties:append('--TEST MODE--')
             properties:append('Bard item = ${barditem|Wind}')
         end
-        properties:append('--Skill Up--')
-        if gs_skillup.use_trust then
-            properties:append('\\crUsing Moogle Trust')
-        end
-        if gs_skillup.use_geo then
-            properties:append("\\crUsing Geo's Refresh")
-        end
-        if gs_skillup.use_item then
-            properties:append('\\crUsing Skill Up Item')
-        end
         properties:append('Mode :\n   ${mode}')
         if gs_skill.skillup_type == 'Singing' then
             properties:append('\\crCurrent Singing Skill LVL:\n   ${skillssing|0}')
             properties:append('\\crCurrent String Skill LVL:\n   ${skillstring|0}')
             properties:append('\\crCurrent Wind Skill LVL:\n   ${skillwind|0}')
-        elseif gs_skill.skillup_type == 'Geomancy' then
-            properties:append('\\crCurrent Geomancy Skill LVL:\n   ${skillgeo|0}')
-            properties:append('\\crCurrent Handbell Skill LVL:\n   ${skillbell|0}')
         else
             properties:append('\\crCurrent Skilling LVL:\n   ${skillbulk|0}')
         end
@@ -419,16 +297,13 @@ function initialize(text, settings, a)
     end
     if a == 'button' then
         local properties = L{}
-        properties:append('${TRUSTc}')
         properties:append('${REFc}')
-        properties:append('${ITEMc}')
         properties:append('${HELc}')
         properties:append('${ENHc}')
         properties:append('${NINc}')
         properties:append('${SINc}')
         properties:append('${BLUc}')
         properties:append('${SMNc}')
-        properties:append('${GEOc}')
         properties:append('${STOPc}')
         properties:append('${DOWNc}')
         properties:append('${LOGc}')
@@ -447,8 +322,6 @@ function updatedisplay()
         info.skillssing = (gs_skillup.skill['Singing Capped'] and "Capped" or gs_skillup.skill['Singing Level'])
         info.skillstring = (gs_skillup.skill['Stringed Instrument Capped'] and "Capped" or gs_skillup.skill['Stringed Instrument Level'])
         info.skillwind = (gs_skillup.skill['Wind Instrument Capped'] and "Capped" or gs_skillup.skill['Wind Instrument Level'])
-        info.skillgeo = (gs_skillup.skill['Geomancy Capped'] and "Capped" or gs_skillup.skill['Geomancy Level'])
-        info.skillbell = (gs_skillup.skill['Handbell Capped'] and "Capped" or gs_skillup.skill['Handbell Level'])
         info.skill = {}
         info.skill.Healing = (gs_skillup.skill['Healing Magic Capped'] and "Capped" or gs_skillup.skill['Healing Magic Level'])
         info.skill.Enhancing = (gs_skillup.skill['Enhancing Magic Capped'] and "Capped" or gs_skillup.skill['Enhancing Magic Level'])
@@ -459,7 +332,6 @@ function updatedisplay()
         info.type = end_skillup.stoptype
         info.skill_ph = (get_rate(gs_skillup.skill_ups) or 0) / 10
         info.skill_total = (gs_skillup.total_skill_ups or 0) / 10
-        info.GEOc = (gs_skillup.color.GEO and 'Start GEO' or '\\cs(255,0,0)Start GEO\\cr')
         info.HELc = (gs_skillup.color.HEL and 'Start Healing' or '\\cs(255,0,0)Start Healing\\cr')
         info.ENHc = (gs_skillup.color.ENH and 'Start Enhancing' or '\\cs(255,0,0)Start Enhancing\\cr')
         info.NINc = (gs_skillup.color.NIN and 'Start Ninjutsu' or '\\cs(255,0,0)Start Ninjutsu\\cr')
@@ -469,9 +341,6 @@ function updatedisplay()
         info.STOPc = (gs_skillup.color.STOP and 'Stop Skillups' or '\\cs(255,0,0)Stop Skillups\\cr')
         info.DOWNc = (gs_skillup.color.DOWN and 'Shutdown After Skillup' or '\\cs(255,0,0)Shutdown After Skillup\\cr')
         info.LOGc = (gs_skillup.color.LOG and  'Logoff After Skillup' or '\\cs(255,0,0)Logoff After Skillup\\cr')
-        info.TRUSTc = (gs_skillup.color.TRUST and  'Use Moogle Trust' or '\\cs(255,0,0)Use Moogle Trust\\cr')
-        info.REFc = (gs_skillup.color.REF and  "Use Geo's Refresh" or "\\cs(255,0,0)Use Geo's Refresh\\cr")
-        info.ITEMc = (gs_skillup.color.ITEM and  'Use Skill Up Item' or '\\cs(255,0,0)Use Skill Up Item\\cr')
         info.TESTc = (gs_skillup.color.TEST and  'Change Bard Item' or '\\cs(255,0,0)Change Bard Item\\cr')
         info.barditem = gs_skillup.test_brd
         button:update(info)
@@ -520,11 +389,6 @@ windower.raw_register_event('incoming chunk', function(id, data, modified, injec
         gs_skillup.skill = packet
         updatedisplay()
     end
-    if id == 0x0DF and skilluprun then
-        if data:unpack('I', 0x0D) == player.max_mp and skilluprun then
-            windower.send_command('input /heal off')
-        end
-    end
 end)
 windower.raw_register_event('mouse', function(type, x, y, delta, blocked)
     local mx, my = texts.extents(button)
@@ -551,8 +415,8 @@ windower.raw_register_event('mouse', function(type, x, y, delta, blocked)
         elseif button:hover(x, y) and button:visible() then
             window:pos((gs_skillup.boxa.pos.x + 145), gs_skillup.boxa.pos.y)
             for i, v in ipairs(location) do
-                local switch = {[1]="TRUST",[2]='REF',[3]='ITEM',[4]="HEL",[5]="ENH",[6]="NIN",[7]="SIN",[8]="BLU",[9]="SMN",[10]="GEO",[11]="STOP",[12]="DOWN",
-                                [13]="LOG",[14]="TEST"}
+                local switch = {[1]='REF',[2]="HEL",[3]="ENH",[4]="NIN",[5]="SIN",[6]="BLU",[7]="SMN",[8]="STOP",[8]="DOWN",
+                                [9]="LOG",[10]="TEST"}
                 if hy > location[i].ya and hy < location[i].yb then
                     set_color(switch[i])
                     updatedisplay()
@@ -565,9 +429,9 @@ windower.raw_register_event('mouse', function(type, x, y, delta, blocked)
     elseif type == 2 then
         if button:hover(x, y) and button:visible() then
             for i, v in ipairs(location) do
-                local switchb = {[1]="settrust",[2]="setgeo",[3]="setitem",[4]="start Healing",[5]="start Enhancing",[6]="start Ninjutsu",
-                                [7]="start Singing",[8]="start Blue",[9]="start Summoning",[10]="start Geomancy",[11]="skillstop",[12]="aftershutdown",
-                                [13]="afterlogoff",[14]="changeinstrament"}
+                local switchb = {[1]="start Healing",[2]="start Enhancing",[3]="start Ninjutsu",
+                                [4]="start Singing",[5]="start Blue",[6]="start Summoning",[7]="skillstop",[8]="aftershutdown",
+                                [9]="afterlogoff",[10]="changeinstrument"}
                 if hy > location[i].ya and hy < location[i].yb then
                     send_command("gs c "..switchb[i])
                     updatedisplay()
